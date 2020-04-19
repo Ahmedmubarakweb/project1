7ae1a07a18969ea11916ed4198231ea0784e33a0
Ahmedmubarakweb <58855051+Ahmedmubarakweb@users.noreply.github.com>
2020-04-19 15:08:20 -0700

    Add files via upload

---
+++ js/index.js
@@ -0,0 +1,46 @@
+
+  $('.cards').slick({
+    dots: true,
+    infinite: true,
+    speed: 300,
+    slidesToShow:1 ,
+    slidesToScroll:1,
+    responsive: [
+      {
+        breakpoint: 992,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+          infinite: true,
+          dots: true
+        }
+      },
+      {
+        breakpoint: 768,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+          infinite: true,
+          dots: true
+        }
+      },
+      {
+        breakpoint: 425,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1
+        }
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1
+        }
+      }
+      // You can unslick at a given breakpoint now by adding:
+      // settings: "unslick"
+      // instead of a settings object
+    ]
+  });
+    
